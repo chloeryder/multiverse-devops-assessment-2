b41627137e2f36a06d0b7a5aedbc90216e2765eb
chloeryder <chloeryder@hotmail.co.uk>
2023-03-25 01:24:01 +0000

    testing file path for filename in input_script

--- survey_app/input_script.py
+++ survey_app/input_script.py
@@ -4,7 +4,7 @@ from extract import get_input, get_cleaned_input, get_formatted_input, validate_
 
 def main():
     
-    filename = 'results.csv'
+    filename = 'survey_app/results.csv'
 
     input_list = get_input(filename)
     cleaned_list = get_cleaned_input(input_list)

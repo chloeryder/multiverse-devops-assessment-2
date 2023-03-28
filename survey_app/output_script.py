94fea2682586fc4ba8caed6a30efbc773a6c1640
chloeryder <chloeryder@hotmail.co.uk>
2023-03-28 11:49:05 +0100

    changes to input and output script

--- survey_app/output_script.py
+++ survey_app/output_script.py
@@ -1,4 +1,10 @@
+import sys
 from extract import get_input, print_final_output
 
-final_results = get_input('./clean_results.csv')
-print_final_output(final_results)
+#Ticket 7
+def output_main():
+    final_results = get_input('./clean_results.csv')
+    print_final_output(final_results)
+
+if __name__ == '__main__':
+    sys.exit(output_main())
